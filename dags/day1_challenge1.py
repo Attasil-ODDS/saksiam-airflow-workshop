ba129977c2e6fd175696aa5d123ef3b07a015e89
ProjectCoder <acgodsleeper@gmail.com>
2021-09-27 23:18:00 +0700

    add new task dependencies challenge1

--- dags/day1_challenge1.py
+++ dags/day1_challenge1.py
@@ -25,7 +25,11 @@ with DAG(
   node8 = DummyOperator(task_id="8")
   node9 = DummyOperator(task_id="9")
 
-  node1 >> node2 >> node3 >> node4 >> node9
-  node2 >> node6 >> node8 >> node9
-  node1 >> node5 >> [node6, node7] >> node8 >> node9
+  # node1 >> node2 >> node3 >> node4 >> node9
+  # node2 >> node6 >> node8 >> node9
+  # node1 >> node5 >> [node6, node7] >> node8 >> node9
+
+  node1 >> [node2, node5] >> node6 >> node8 >> node9
+  node2 >> node3 >> node4 >> node9
+  node5 >> node7 >> node8
 
